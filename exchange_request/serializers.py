dd58dc4df4c1bfbdd512da191f9e442748a27815
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2024-10-16 00:48:37 +0530

    Update serializers.py

--- exchange_request/serializers.py
+++ exchange_request/serializers.py
@@ -2,9 +2,28 @@ from rest_framework import serializers
 from .models import ExchangeRequest
 
 class ExchangeRequestSerializer(serializers.ModelSerializer):
+    initiator_name = serializers.CharField(read_only=True)
+    acceptor_name = serializers.CharField(read_only=True)
+    initiator_book_title = serializers.CharField(read_only=True)
+    acceptor_book_title = serializers.CharField(read_only=True)
+
     class Meta:
         model = ExchangeRequest
-        fields = '__all__'
+        fields = [
+            'exchange_id', 
+            'initiator', 
+            'acceptor', 
+            'initiator_name', 
+            'acceptor_name', 
+            'initiator_book', 
+            'acceptor_book', 
+            'initiator_book_title', 
+            'acceptor_book_title', 
+            'created_on', 
+            'delivery_method', 
+            'exchange_duration', 
+            'request_status'
+        ]
 
 class ExchangeRequestCreateSerializer(serializers.ModelSerializer):
     class Meta:
