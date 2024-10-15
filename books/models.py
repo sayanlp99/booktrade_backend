0a962ed19145de765353db304403f4a72434600b
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2024-10-15 14:47:05 +0530

    Add part for getting books from a user, and also search for books in the platform

--- books/models.py
+++ books/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from authentication.models import UserProfile
+import uuid
 
 class Book(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     genre = models.CharField(max_length=50)
     condition = models.CharField(max_length=50)
     availability_status = models.BooleanField(default=True)
     owner = models.UUIDField()
-
-    def __str__(self):
-        return self.title
--- books/views.py
+++ books/views.py
@@ -5,6 +5,7 @@ from .serializers import *
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 from authentication.models import UserProfile
+from django.db.models import Q
 
 class BookViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
@@ -15,7 +16,7 @@ class BookViewSet(viewsets.ModelViewSet):
     def list(self, request, *args, **kwargs):
         try:
             user_profile = UserProfile.objects.get(username=request.user.username)
-            queryset = Book.objects.filter(user_uuid=user_profile.uuid)
+            queryset = Book.objects.filter(owner=user_profile.uuid)
             serializer = BookSerializer(queryset, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
@@ -23,10 +24,11 @@ class BookViewSet(viewsets.ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         try:
-            serializer = Book(data=request.data)
+            print(request.data)
+            serializer = BookSaveSerializer(data=request.data)
             if serializer.is_valid():
                 user_profile = UserProfile.objects.get(username=request.user.username)
-                serializer.save(user_uuid=user_profile.uuid)
+                serializer.save(owner=user_profile.uuid)
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
@@ -36,9 +38,8 @@ class BookViewSet(viewsets.ModelViewSet):
         try:
             instance = self.get_object()
             user_profile = UserProfile.objects.get(username=request.user.username)
-            if instance.user_uuid != user_profile.uuid:
+            if instance.owner != user_profile.uuid:
                 return Response({'error': 'You do not have permission to perform this action.'}, status=status.HTTP_403_FORBIDDEN)
-
             serializer = BookSaveSerializer(instance, data=request.data)
             if serializer.is_valid():
                 serializer.save()
@@ -51,10 +52,34 @@ class BookViewSet(viewsets.ModelViewSet):
         try:
             instance = self.get_object()
             user_profile = UserProfile.objects.get(username=request.user.username)
-            if instance.user_uuid != user_profile.uuid:
+            if instance.owner != user_profile.uuid:
                 return Response({'error': 'You do not have permission to perform this action.'}, status=status.HTTP_403_FORBIDDEN)
-
             instance.delete()
             return Response({'message': 'Deleted the item from the Book list'}, status=status.HTTP_204_NO_CONTENT)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+    
+    def get_books_by_user(self, request):
+        try:
+            user_id = request.query_params.get('userId')
+            if not user_id:
+                return Response({"error": "userId parameter is required"}, status=status.HTTP_400_BAD_REQUEST)        
+            books = Book.objects.filter(owner=user_id)
+            serializer = BookSerializer(books, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+    
+    def search_books(self, request):
+        keyword = request.query_params.get('q')
+        if not keyword:
+            return Response({"error": "Search keyword 'q' is required"}, status=status.HTTP_400_BAD_REQUEST)
+        books = Book.objects.filter(
+            Q(title__icontains=keyword) | 
+            Q(author__icontains=keyword) | 
+            Q(genre__icontains=keyword)
+        )
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
